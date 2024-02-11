eb6f810d398e24b80db5e7d92dc7f1e722b51ba4
orukahairuka <sakuraierika03301109@gmail.com>
2024-02-11 11:45:36 +0900

    build

---
+++ mapppp1234/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mapppp1234
+//
+//  Created by 櫻井絵理香 on 2024/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mapppp1234/mapppp1234App.swift
@@ -0,0 +1,17 @@
+//
+//  mapppp1234App.swift
+//  mapppp1234
+//
+//  Created by 櫻井絵理香 on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct mapppp1234App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
