4a35d8355c258041579e477fb9c31a19c4266fdc
orukahairuka <sakuraierika03301109@gmail.com>
2024-02-11 16:26:38 +0900

    fix:TextField

--- mapppp1234/ContentView.swift
+++ mapppp1234/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    //入力中の文字列を保持する状態
+    @State var inputText: String = ""
+    //検索キーワードを保持する状態
+    @State var displaySearchKey = ""
     var body: some View {
         VStack {
-            MapView(searchKey: "羽田空港")
+            //テキストフィールド(文字入力)
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+            //入力が完了されたとき
+                .onSubmit {
+                    //入力が完了したので検索キーワードに設定する
+                    displaySearchKey = inputText
+                }
+                .padding()
+            MapView(searchKey: displaySearchKey)
         }
     }
 }
